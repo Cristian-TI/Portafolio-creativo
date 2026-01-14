4189127324282878c5e5239fdd73486a14afef65
Cristian-TI <venturacristian051006@gmail.com>
2026-01-14 00:55:19 -0600

    add: se agrego un menu

---
+++ js/java.js
@@ -0,0 +1,17 @@
+const reveals = document.querySelectorAll('.reveal');
+
+function revealOnScroll() {
+    const windowHeight = window.innerHeight;
+    const revealPoint = 100;
+
+    reveals.forEach(el => {
+        const elementTop = el.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - revealPoint) {
+            el.classList.add('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', revealOnScroll);
+window.addEventListener('load', revealOnScroll);
